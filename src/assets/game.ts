9449263656e1be36e8e63db73262c28925d8118c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-16 15:19:50 +0000

    Updated game.ts - added event listener

--- src/assets/game.ts
+++ src/assets/game.ts
@@ -5,12 +5,24 @@ export default class Game {
     width: number;
     height: number;
     player: Player;
+    keys: string[];
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.keys = [];
         this.player = new Player(this);
+
+        //event listeners
+        window.addEventListener('keydown', e => {
+            if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+        })
+
+        window.addEventListener('keyup', e => {
+            const index = this.keys.indexOf(e.key);
+            if (index > -1) this.keys.splice(index, 1);
+        })
     }
     render(context: CanvasRenderingContext2D) {
         this.player.draw(context);
