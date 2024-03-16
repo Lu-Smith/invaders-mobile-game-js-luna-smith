b107cb595aeff6b9235502489c44713e4821dee3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-16 14:48:01 +0000

    Updated player.ts

--- src/assets/player.ts
+++ src/assets/player.ts
@@ -6,15 +6,20 @@ export default class Player {
     height: number;
     x: number;
     y: number;
+    speed: number;
 
     constructor(game: Game) {
         this.game = game;
-        this.width = 100;
-        this.height = 100;
-        this.x = 200;
-        this.y = 200;
+        this.width = 60;
+        this.height = 60;
+        this.x = this.game.width * 0.5 - this.width * 0.5;
+        this.y = this.game.height - this.height;
+        this.speed = 5;
     }
     draw(context: CanvasRenderingContext2D) {
         context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update() {
+        this.x += this.speed;
     }
 }
