dcbe71813d4ec80776d742d25d565f9a3cbfbf12
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-16 14:17:53 +0000

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import './style.css';
+import Game from './assets/game';
 
 window.addEventListener('load', function() {
   const canvas = document.getElementById('canvas1') as HTMLCanvasElement;
   if (canvas) {
     const ctx = canvas.getContext('2d');
-    canvas.width = 600;
-    canvas.height = 800;
+    canvas.width = 300;
+    canvas.height = 400;
+
+    const game = new Game(canvas);
+    console.log(game);
   }
 });
 
