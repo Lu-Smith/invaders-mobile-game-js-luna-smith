282d3eb95e4eef6edff488a7ae336845a6372109
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-16 14:17:46 +0000

    Updated game.ts

--- src/assets/game.ts
+++ src/assets/game.ts
@@ -1,3 +1,14 @@
-class Game {
-    
+export default class Game {
+    canvas: HTMLCanvasElement;
+    width: number;
+    height: number;
+
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+    }
+    render() {
+        console.log(this.width, this.height);
+    }
 }
