90fca87c1abe06373bf3be22db7c601b90e1f0b6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-18 19:54:31 +0000

    Updated projectile.ts

--- src/assets/projectile.ts
+++ src/assets/projectile.ts
@@ -1,3 +1,13 @@
-class Projectile {
+export default class Projectile {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
     
+    constructor() {
+        this.width = 4;
+        this.height = 20;
+        this.x = 0;
+        this.y = 0;
+    }
 }
