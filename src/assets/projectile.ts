56b3ca5bb533e758338d65c44914867f3f683ccf
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-18 19:59:25 +0000

    Updated projectile.js

--- src/assets/projectile.ts
+++ src/assets/projectile.ts
@@ -3,11 +3,25 @@ export default class Projectile {
     height: number;
     x: number;
     y: number;
-    
+    speed: number;
+    free: boolean;
+
     constructor() {
         this.width = 4;
         this.height = 20;
         this.x = 0;
         this.y = 0;
+        this.speed = 20;
+        this.free = true;
+    }
+    draw() {
+        if(!this.free) {
+
+        }
+    }
+    update() {
+        if(!this.free) {
+            
+        }
     }
 }
