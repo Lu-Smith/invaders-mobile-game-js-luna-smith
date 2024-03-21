44861f07899bfcabbf35920cd7f840e72e310cf1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-21 21:26:06 +0000

    Updated enemy.ts constructor

--- src/assets/enemy.ts
+++ src/assets/enemy.ts
@@ -1,3 +1,17 @@
-class Enemy {
-    
+import Game from './game';
+
+export default class Enemy {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+
+    game: Game;
+    constructor(game: Game) {
+        this.game = game;
+        this.width = 0;
+        this.height = 0;
+        this.x = 10;
+        this.y = 10;
+    }
 }
