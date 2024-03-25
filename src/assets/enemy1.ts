20a5b5f6fa05409b9928efe5baae39a8e161aa80
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-25 19:26:19 +0000

    Updated enemy1.ts

--- src/assets/enemy1.ts
+++ src/assets/enemy1.ts
@@ -2,9 +2,14 @@ import Enemy from './enemy';
 import Game from './game';
 
 export default class Enemy1 extends Enemy {
+    image: HTMLImageElement;
+    frameX: number;
+    frameY: number;
+
     constructor(game: Game, positionX: number, positionY: number) {
         super(game, positionX, positionY);
-        this.image = document.getElementById('enemy1')
-
+        this.image = document.getElementById('enemy1') as HTMLImageElement;
+        this.frameX = 0;
+        this.frameY = 0;
     }
 }
