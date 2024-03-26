6cb3b1f868180b4b387b9061f2d3587ab8ba990b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-26 12:32:27 +0000

    Added maxLives to enemy.ts

--- src/assets/enemy.ts
+++ src/assets/enemy.ts
@@ -46,17 +46,16 @@ export default class Enemy {
                 }
             }
         }
-        // check collision enemies - player
-        if (this.game.checkCollision(this, this.game.player)) {
-            this.markedForDeletion = true;
-            if (!this.game.gameOver && this.game.score > 0) this.game.score--;
-            this.game.player.lives--;
-            if (this.game.player.lives < 1) this.game.gameOver = true;
+        // check collision enemies-player
+        if (this instanceof Enemy1) {
+            if (this.game.checkCollision(this, this.game.player) && this.lives > 0) {
+                this.lives = 0;
+                this.game.player.lives--;
+            }
         }
         // lose condition
-        if (this.y + this.height > this.game.height) {
+        if (this.y + this.height > this.game.height || this.game.player.lives < 1) {
             this.game.gameOver = true;
-            this.markedForDeletion = true;
         }
     }
     hit(damage: number) {
