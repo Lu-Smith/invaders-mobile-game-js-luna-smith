5136be1f310767baa25c1a12e595e3f60f746d51
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-26 10:43:39 +0000

    Updated enemy1.ts

--- src/assets/enemy1.ts
+++ src/assets/enemy1.ts
@@ -5,11 +5,17 @@ export default class Enemy1 extends Enemy {
     image: HTMLImageElement;
     frameX: number;
     frameY: number;
+    lives: number;
+    maxFrame: number;
+    maxLives: number;
 
     constructor(game: Game, positionX: number, positionY: number) {
         super(game, positionX, positionY);
         this.image = document.getElementById('enemy1') as HTMLImageElement;
         this.frameX = 0;
-        this.frameY = 0;
+        this.maxFrame = 2;
+        this.frameY = Math.floor(Math.random() * 4);
+        this.lives = 1;
+        this.maxLives = this.lives;
     }
 }
